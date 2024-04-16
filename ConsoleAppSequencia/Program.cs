a88356fe9be21c5eea639069625f01cfeed7273a
Usuario <Usuario@CPD02-M02>
2024-04-16 11:36:24 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppSequencia/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSequencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <=9; i++)
+            {
+                Console.Write(i);
+                for(int j = 1; j <=9; j++)
+                {
+                    if (i != j)
+                    {
+                            Console.Write(j);
+                    }                                      
+                }      
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
